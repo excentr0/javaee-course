4e482ecd094346332b6130335976725e12931de5
Mikhail Mishyn <excentro@gmail.com>
2021-03-18 13:49:39 +0300

    http servlet

---
+++ firstapp/src/main/java/org/excentro/FirstHTTPServlet.java
@@ -0,0 +1,17 @@
+package org.excentro;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "http-servlet")
+public class FirstHTTPServlet extends HttpServlet {
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+    resp.getWriter().println("Hello from HTTP Sevlet");
+  }
+}
